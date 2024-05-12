09de616b2c3a6cb1d4695dffbaba6e24bb705831
Manya Malhotra <malhotra.manyaaaa@gmail.com>
2024-05-12 03:14:36 +0100

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Manya Malhotra on 12/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
